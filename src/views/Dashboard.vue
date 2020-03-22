175fda9e65cdb4f08c2fdda64a9e0da6dd234c8b
Felipe Carvalho <karvalho_182@hotmail.com>
2020-03-22 19:34:03 -0300

    feature: implements first conditional to portal-percentage chart using apexCharts

--- src/views/Dashboard.vue
+++ src/views/Dashboard.vue
@@ -4,12 +4,32 @@
   <div class="dashboard__container">
     <div class="dashboard__percentage-portal">
       <div class="chart-type-pie">
-      <apexcharts
-      heigth="245"
-      type="pie"
-      :options="chartOptions"
-      :series="series">
-      </apexcharts>
+        <apexchart
+        heigth="245"
+        type="donut"
+        :options="this.percentagePortal.options"
+        :series="this.percentagePortal.series">
+        </apexchart>
+      </div>
+    </div>
+    <div class="dashboard__percentage-portal">
+      <div class="chart-type-pie">
+        <apexchart
+        heigth="245"
+        type="donut"
+        :options="this.percentagePortal.options"
+        :series="this.percentagePortal.series">
+        </apexchart>
+      </div>
+    </div>
+    <div class="dashboard__percentage-portal">
+      <div class="chart-type-pie">
+        <apexchart
+        heigth="245"
+        type="donut"
+        :options="this.percentagePortal.options"
+        :series="this.percentagePortal.series">
+        </apexchart>
       </div>
     </div>
   </div>
@@ -18,9 +38,10 @@
 
 <script lang="ts">
 import Vue from 'vue';
-import ApexCharts from 'apexcharts';
+import VueApexCharts from 'vue-apexcharts';
+import axios from 'axios';
 
-Vue.component('apexcharts', ApexCharts);
+Vue.component('apexchart', VueApexCharts);
 
 export default Vue.extend({
   name: 'Dashboard',
@@ -29,16 +50,51 @@ export default Vue.extend({
   },
   data() {
     return {
-      chartOptions: [50, 50],
-      series: ['A', 'B'],
+      percentagePortal: {
+        options: {
+          labels: [],
+        },
+        series: [],
+      },
     };
   },
   methods: {
+    loadData() {
+      axios.get('http://www.mocky.io/v2/5e42ebca2f0000013687fa64')
+        .then((response) => {
+          const portals = response.data.map((res) => res.Portal);
+          const novamente = portals.reduce((prev: { [x: string]: number }, cur: string) => {
+            const oi = prev;
+            oi[cur] = (oi[cur] || 0) + 1;
+            return oi;
+          }, {});
 
+          Object.keys(novamente).forEach((e) => {
+            this.percentagePortal.options.labels.push(e);
+            this.percentagePortal.series.push(novamente[e]);
+          });
+
+          console.log(this.percentagePortal);
+        });
+    },
+  },
+  mounted() {
+    this.loadData();
   },
 });
 </script>
 
 <style lang="scss" scoped>
+  .dashboard {
+    width: 100%;
+    display: flex;
+    flex-direction: column;
+    height: 100vh;
+    overflow: auto;
 
+    &__percentage-portal {
+      width: 40%;
+      margin: 0 auto;
+    }
+  }
 </style>
